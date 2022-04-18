93444b32b673a88e47e196667ff25013af6212a7
luanmoraes123 <luan.moraees21@gmail.com>
2022-04-18 08:55:20 -0300

    Category

---
+++ src/main/java/com/luanmoraes/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.luanmoraes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luanmoraes.course.entities.Category;
+import com.luanmoraes.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
