30b24ff03df74b0e57d8d57051098b4ab22ae190
luanmoraes123 <luan.moraees21@gmail.com>
2022-04-19 13:27:10 -0300

    User update

--- src/main/java/com/luanmoraes/course/repositories/OrderItemRepository.java
+++ src/main/java/com/luanmoraes/course/repositories/OrderItemRepository.java
@@ -3,7 +3,6 @@ package com.luanmoraes.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luanmoraes.course.entities.OrderItem;
-import com.luanmoraes.course.entities.User;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 
--- src/main/java/com/luanmoraes/course/repositories/UserRepository.java
+++ src/main/java/com/luanmoraes/course/repositories/UserRepository.java
@@ -1,10 +1,7 @@
 package com.luanmoraes.course.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.luanmoraes.course.entities.OrderItem;
 import com.luanmoraes.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
